ba06e8465b185e8dbc8a36d18e63db3b7e12a6c5
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2024-07-17 01:22:09 +0500

    API urls

--- todo/backend/api/urls.py
+++ todo/backend/api/urls.py
@@ -1,3 +1,7 @@
 from django.urls import path
+from .views import TodoList, TodoDetail
 
-urlpatterns = []
+urlpatterns = [
+    path('', TodoList.as_view(), name='todo-list'),
+    path('<int:pk>/', TodoDetail.as_view(), name='todo-detail'),
+]
--- todo/backend/api/views.py
+++ todo/backend/api/views.py
@@ -1,2 +1,26 @@
-from django.shortcuts import render
 from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.generics import ListAPIView, RetrieveAPIView, DestroyAPIView
+from rest_framework import status
+
+from todo.models import Todo
+from .serializers import ToDoSerializer
+
+
+class TodoList(APIView):
+    def get(self, request, format=None):
+        queryset = Todo.objects.all()
+        serializer = ToDoSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = ToDoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class TodoDetail(RetrieveAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = ToDoSerializer
