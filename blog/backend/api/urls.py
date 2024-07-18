6f35b7a78d4700cc05b16d7b4bcfe29912640175
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2024-07-18 20:24:21 +0500

    permissions

--- blog/backend/api/urls.py
+++ blog/backend/api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import PostListAPIView, PostDetailAPIView
 
 urlpatterns = [
-    path('<int:pk>/', PostDetailAPIView.as_view(), name='post-detail'),
+    path('/<int:pk>/', PostDetailAPIView.as_view(), name='post-detail'),
     path('', PostListAPIView.as_view(), name='post-list'),
 ]
--- blog/backend/api/views.py
+++ blog/backend/api/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 from blog.models import Post
 from .serializers import PostSerializer
-
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from .permissions import IsOwnerOrReadOnly
 
 
 class PostListAPIView(ListCreateAPIView):
@@ -14,16 +14,8 @@ class PostListAPIView(ListCreateAPIView):
     serializer_class = PostSerializer
     permission_classes = (IsAuthenticated,)
 
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-
+   
 class PostDetailAPIView(RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = (IsAuthenticated,)
-
-    def perform_update(self, serializer):
-        serializer.save(author=self.request.user)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+    permission_classes = (IsOwnerOrReadOnly,)
